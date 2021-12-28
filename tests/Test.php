99e0aa981d1501af88e4b2935de775acc0f8a1a3
Sofiakb <contact.sofiakb@gmail.com>
2021-12-28 10:37:39 +0100

    Tests

--- tests/Test.php
+++ tests/Test.php
@@ -5,5 +5,13 @@ use PHPUnit\Framework\TestCase;
 
 class Test extends TestCase
 {
-
+    
+    public function testEmail()
+    {
+        $this->assertTrue(\Sofiakb\Mail\Facades\Mail::create()
+                ->setTo('contact.sofiakb@gmail.com')
+                ->subject('sofiakb/php-mail')
+                ->text('Message de test'));
+    }
+    
 }
